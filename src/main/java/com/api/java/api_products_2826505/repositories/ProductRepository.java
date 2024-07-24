a2aeb1f2fb44df19c1297a1fb8b9d46130004c0f
unknown <Aprendiz@BOGDFPCGMS4136.FormacionSena.RED>
2024-07-24 17:42:30 -0500

    repositorio guardado

--- src/main/java/com/api/java/api_products_2826505/repositories/ProductRepository.java
+++ src/main/java/com/api/java/api_products_2826505/repositories/ProductRepository.java
@@ -1,9 +1,15 @@
 package com.api.java.api_products_2826505.repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import com.api.java.api_products_2826505.entities.Product;
 
+@Repository
 public interface ProductRepository extends CrudRepository<Product , Long >
 {
 //repositorio por cada entidad 
+
+
+  //crear metodos para la CRUD
+    //que estan en el respositorio 
 }
